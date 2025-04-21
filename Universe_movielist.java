7766527f438b53b047caa87e1125f97463e4f7b7
faiz454 <mohammedfaizck454@gmail.com>
2025-04-21 15:39:36 +0530

    Add files via upload

---
+++ Universe_movielist.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.ArrayList;
+class Universe_movielist {
+    public static void main(String args[]){
+        System.out.println("LCU");
+        System.out.println("----");
+        List<String>movies=new ArrayList<>();
+        movies.add("1.Kaithi");
+        movies.add("2.Vikram");
+        movies.add("3.Leo");
+        System.out.println(movies);
+    }
+}
